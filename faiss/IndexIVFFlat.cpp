b8c0f81894cc04549b1cfce8c294f4b6c2185ec5
Derrick Quinn <dq55@cornell.edu>
2025-01-28 18:09:33 +0000

    Fix previous

--- faiss/IndexIVFFlat.cpp
+++ faiss/IndexIVFFlat.cpp
@@ -164,11 +164,11 @@ struct IVFFlatScanner : InvertedListScanner {
                 query_base + queries[i] * d,
                 d * float_size);
         }
-
+        
         if (store_new_xi) {
         delete[] xi;
     }
-
+        
         this->xi = new_xi;
         nx = queries.size();
         store_new_xi = true;
@@ -256,7 +256,12 @@ struct IVFFlatScanner : InvertedListScanner {
         }
     }
 
-
+    
+    ~IVFFlatScanner() override {
+        if (store_new_xi) {
+            delete[] xi;
+        }
+    }
 };
 
 template <bool use_sel>
