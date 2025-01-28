d42c96035cedcb5dc78979cc3a4e173aa33f1926
Derrick Quinn <dq55@cornell.edu>
2025-01-28 18:02:53 +0000

    Comment out memory management code in IVFFlatScanner to prevent potential issues

--- faiss/IndexIVFFlat.cpp
+++ faiss/IndexIVFFlat.cpp
@@ -164,9 +164,11 @@ struct IVFFlatScanner : InvertedListScanner {
                 query_base + queries[i] * d,
                 d * float_size);
         }
+        /*
         if (store_new_xi) {
         delete[] xi;
     }
+        */
         this->xi = new_xi;
         nx = queries.size();
         store_new_xi = true;
