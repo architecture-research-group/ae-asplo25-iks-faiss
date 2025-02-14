90bff5b3665f1ae7f7297d14efb619bd395b5986
Derrick Quinn <dq55@cornell.edu>
2025-02-14 22:32:44 +0000

    Removed duplicate functions.

--- faiss/IndexIVF.cpp
+++ faiss/IndexIVF.cpp
@@ -1389,17 +1389,6 @@ size_t InvertedListScanner::scan_codes(
     return nup;
 }
 
-size_t InvertedListScanner::scan_codes_batched(
-    size_t n,
-    const uint8_t* codes,
-    const idx_t* ids,
-    float* distances,
-    idx_t* labels,
-    size_t k) const {
-FAISS_THROW_MSG("scan_codes_batched not implemented");
-return 0;
-}
-
 
 size_t InvertedListScanner::iterate_codes(
         InvertedListsIterator* it,
